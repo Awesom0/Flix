bcce403a9c9e9dffcf6f6fe8000e45b47714091b
Felipe De La Torre <felipe9141@sbcglobal.net>
2018-10-04 23:36:58 -0700

    Flix Part 2 complete

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Felipe De La Torre on 10/4/18.
+//  Copyright © 2018 Felipe De La Torre. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
